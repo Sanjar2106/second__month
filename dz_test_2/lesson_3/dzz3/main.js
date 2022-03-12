98b4be80b473d03f686f62770cf046cf8d39cfbb
user <abdykulovs21@gmail.com>
2022-03-12 19:19:07 +1200

    dz33

--- dz_test_2/lesson_3/dzz3/main.js
+++ dz_test_2/lesson_3/dzz3/main.js
@@ -1,15 +1,25 @@
-function sanjar(num, num1) {
-    if (num.length < num1.length){
-        alert('первая меньше')
-    } else if (num.length === num1.length) {
-        alert ('они равны')
-    } else if (num.length > num1.length){
-        alert('первый больше')
+// function sanjar(num, num1) {
+//     if (num.length < num1.length){
+//         alert('первая меньше')
+//     } else if (num.length === num1.length) {
+//         alert ('они равны')
+//     } else if (num.length > num1.length){
+//         alert('первый больше')
+//     }
+// }
+
+// let num = [ 1123, 32, 35, 90, '2', 423423,]
+// let num1 = [ 11, 22, 33, 44, '2', ]
+    
+
+// sanjar(num, num1)
+
+function ssanjar(countChar) {
+    if (countChar.length){
+        console.log(countChar.length)
     }
 }
 
-let num = [ 1123, 32, 35, 90, '2', 423423,]
-let num1 = [ 11, 22, 33, 44, '2', ]
-    
+const countChar = [3, 3, 3, 3, 3, 3, 3 ,3 ,3, 3, 3, 3,3,3,3]
 
-sanjar(num, num1)       
+ssanjar(countChar)
