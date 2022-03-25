053b36c3ae24570efabfe3752773d3d666fe1d52
user <abdykulovs21@gmail.com>
2022-03-26 01:59:37 +1200

    dz6_6_6

--- lesson_6/dz_6/main.js
+++ lesson_6/dz_6/main.js
@@ -1,30 +1,32 @@
-// const massive =[
-//     massive2 = [1,3,5,6,8,4,6,7],
-//     massive3 = [5,3,10,6,8,41,6,78],
-//     massive4 = [8,12,5,16,8,87,6,21],
-//     massive5 = [85,95,5,54,8,4,33,7],
-//     massive6 = [2,7,45,69,58,14,0,22]
-// ]
-// console.log([massive])
+const a = 'a';
+const b = 'b';
+const c = 'c';
+const d = 'd';
+const e = 'e';
 
-//  const obj = {
-//     key1: 'value',
+const all ={
+    ...a, b, c, d, e,
+}
+console.log(all)
 
-//     key2: 'value',
+ const obj = {
+    key1: 'value',
 
-//     key3: 'value',
+    key2: 'value',
 
-//     key4: 'value',
+    key3: 'value',
 
-//     key5: 'value'
-// }
-// const obj2 = {
-//     ...obj,
-//     key1q: "value",
-//     key2q: "value",
-//     key3q: "value"
-// }
-// console.log(obj2)
+    key4: 'value',
+
+    key5: 'value'
+}
+const obj2 = {
+    ...obj,
+    key1q: "value",
+    key2q: "value",
+    key3q: "value"
+}
+console.log(obj2)
 
 const list = [];
 
